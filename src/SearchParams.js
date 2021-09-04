f93ec18b35dddd8aafa9eb767ba6da3a58cc3895
Widzmarc Jean Nesly Phelle <widzmarc@gmail.com>
2021-09-04 18:27:22 -0400

    Add form to searchparams component

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,9 +1,17 @@
 import React from 'react'
 
 function SearchParams() {
+    const location = "Seattle, WA";
+
     return (
         <div className="search-params">
-            
+            <form>
+                <label htmlFor="location">
+                    location
+                    <input id="location" value={location} placeholder="Location" />
+                </label>
+                <button>Submit</button>
+            </form>
         </div>
     )
 }
