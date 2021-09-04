e24f2f95608a8a713a5ee1e3e5070b271ee9882b
Widzmarc Jean Nesly Phelle <widzmarc@gmail.com>
2021-09-04 17:29:54 -0400

    Refactor Pet component to use jsx

--- src/Pet.js
+++ src/Pet.js
@@ -1,9 +1,19 @@
-import React from "react";
+//import React from "react";
+
+// export const Pet = ({ name, animal, breed }) => {
+//   return React.createElement("div", {}, [
+//     React.createElement("h2", {}, name),
+//     React.createElement("h3", {}, animal),
+//     React.createElement("h3", {}, breed),
+//   ]);
+// };
 
 export const Pet = ({ name, animal, breed }) => {
-  return React.createElement("div", {}, [
-    React.createElement("h2", {}, name),
-    React.createElement("h3", {}, animal),
-    React.createElement("h3", {}, breed),
-  ]);
+  return (
+    <div>
+      <h2>{name}</h2>
+      <h3>{animal}</h3>
+      <h3>{breed}</h3>
+    </div>
+  )
 };
