9810e2c67c8e8e595496686e51f075d287a3a7fd
Widzmarc Jean Nesly Phelle <widzmarc@gmail.com>
2021-09-04 19:19:25 -0400

    Add select tag

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import { useState } from 'react';
 
+const ANIMALS = ["bird", "cat", "dog", "rabbit", "reptile"];
+
 function SearchParams() {
     const [location, setLocation] = useState("Seattle, WA")
+    const [animal, setAnimal] = useState("")
 
     return (
         <div className="search-params">
@@ -15,6 +18,22 @@ function SearchParams() {
                       value={location} 
                       placeholder="Location" 
                     />
+                </label>
+                <label htmlFor="animal">
+                    ANIMAL
+                    <select
+                        id="animal"
+                        value = {animal}
+                        onChange={e => setAnimal(e.target.value)}
+                        onBlur={e => setAnimal(e.target.value)}
+                    >
+                        <option />
+                        {
+                            // typically you should not use index as key
+                            ANIMALS.map((animal, key) => <option key={key}>{animal}</option>)
+                        }
+                    </select>
+                
                 </label>
                 <button>Submit</button>
             </form>
