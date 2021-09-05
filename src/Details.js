745e598f1a60aa283546d91c4d7e04db954d4d8a
Widzmarc Jean Nesly Phelle <widzmarc@gmail.com>
2021-09-05 16:24:58 -0400

    Create details page

--- src/Details.js
+++ src/Details.js
@@ -1,5 +1,31 @@
-const Details = () => {
-    return <h2>Hello!</h2>
+/* eslint-disable react/prop-types */
+import { Component } from "react";
+import { withRouter } from "react-router-dom";
+
+class Details extends Component {
+    constructor () {
+        super();
+
+        this.state = { loading: true };
+    }
+
+    componentDidMount () {
+        fetch(`http://pets-v2.dev-apis.com/pets?id=${this.props.match.params.id}`)
+        .then(res => res.json())
+        .then(data => this.setState(Object.assign({ loading: false, }, data.pets[0])))
+    }
+
+    render() {
+        const {animal, breed, city, state, description, name} = this.state;
+        return (
+            <div className="details">
+                <h1>{name}</h1>
+                <h2>{`${animal} - ${breed} - ${city}, ${state}`}</h2>
+                <button>Adopt {name}</button>
+                <p>{description}</p>
+            </div>
+        )
+    }
 }
 
-export default Details;
+export default withRouter(Details);
