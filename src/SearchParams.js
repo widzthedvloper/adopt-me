ae2c4d91db08936e899e02a39adb35804024189b
Widzmarc Jean Nesly Phelle <widzmarc@gmail.com>
2021-09-05 10:57:44 -0400

    Implement form submition functionality

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -29,7 +29,10 @@ const SearchParams = () => {
 
   return (
     <div className="search-params">
-      <form>
+      <form onSubmit={e => {
+          e.preventDefault();
+          requestPets();
+      }}>
         <label htmlFor="location">
           location
           <input
