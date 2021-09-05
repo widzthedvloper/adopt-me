662aaace5a1d60eb0a57841a929556e6a04af676
Widzmarc Jean Nesly Phelle <widzmarc@gmail.com>
2021-09-05 15:48:40 -0400

    Wrap the h1 inside a link

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { StrictMode } from "react";
 import ReactDOM from "react-dom";
 import SearchParams from "./SearchParams";
 import Details from "./Details";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
 
 // const App = () => {
 //   return React.createElement("div", {}, [
@@ -28,8 +28,12 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 const App = () => {
   return (
     <div>
-      <h1>Adopt Me!</h1>
       <Router>
+        <header>
+          <Link to="/">
+            <h1>Adopt Me!</h1>
+          </Link>
+        </header>
         <Switch>
           <Route path="/details/:id">
             <Details />
