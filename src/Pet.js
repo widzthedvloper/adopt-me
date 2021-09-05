c5be782da61844c003fdfd2e6344ef89201a8413
Widzmarc Jean Nesly Phelle <widzmarc@gmail.com>
2021-09-05 11:58:05 -0400

    Add classname info

--- src/Pet.js
+++ src/Pet.js
@@ -20,7 +20,7 @@ export const Pet = ({ name, animal, breed, images, location, id }) => {
       <div className="image-container">
         <img src={hero} alt={name} />
       </div>
-      <div>
+      <div className="info">
         <h1>name</h1>
         <h2>{`${animal} - ${breed} - ${location}`}</h2>
       </div>
