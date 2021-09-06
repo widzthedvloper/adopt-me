afcf4ebe7ee6bc05db44b12f7a0f3753f5e9e097
Widzmarc Jean Nesly Phelle <widzmarc@gmail.com>
2021-09-06 16:37:57 -0400

    Add code to redirect user after 5 seconds

--- src/ErrorBoundarry.js
+++ src/ErrorBoundarry.js
@@ -2,20 +2,26 @@
 /* eslint-disable import/named */
 /* eslint-disable import/namespace */
 import { Component } from "react";
-import { Link } from "react-router-dom";
+import { Link, Redirect } from "react-router-dom";
 
 class ErrorBoundary extends Component {
-    state = { hasError: false}
+    state = { hasError: false, redirect: false }
     static getDerivedStateFromError() {
         return { hasError: true }
     }
 
     componentDidCatch(error, info) {
         console.error(' ErrorBoundary caught an error', error, info);
+        if(this.state.hasError) {
+            setTimeout(() => this.setState({redirect: true}), 5000)
+        }
     }
 
     render () {
-        if(this.state.hasError) {
+
+        if(this.state.redirect) {
+            return <Redirect to="/" />
+        }else if(this.state.hasError) {
             return(
                 <h2>
                     This listing has an error. <Link to="/">Click here</Link> to go back to the home page.
