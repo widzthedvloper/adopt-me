1f914b6bdca45d8cecf1dea5b924c1df510fd55b
Widzmarc Jean Nesly Phelle <widzmarc@gmail.com>
2021-09-06 16:26:00 -0400

    Implement error boundaries

--- src/Details.js
+++ src/Details.js
@@ -4,6 +4,7 @@
 import { Component } from "react";
 import { withRouter } from "react-router-dom";
 import Carousel from "./Carousel";
+import ErrorBoundary from "./ErrorBoundarry";
 
 class Details extends Component {
 
@@ -25,6 +26,7 @@ class Details extends Component {
 
 
     const { animal, breed, city, state, description, name, images } = this.state;
+
     return (
       <div className="details">
         <Carousel images={images} />
@@ -39,4 +41,12 @@ class Details extends Component {
   }
 }
 
-export default withRouter(Details);
+const DetailsWithRouter = withRouter(Details);
+
+export default function DetailsWithErrorBoundery() {
+  return(
+    <ErrorBoundary>
+      <DetailsWithRouter />
+    </ErrorBoundary>
+  )
+}
