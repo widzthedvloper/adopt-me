65516f03cd75de51cda0debe6131410f9580ca7f
Widzmarc Jean Nesly Phelle <widzmarc@gmail.com>
2021-09-07 19:28:17 -0400

    Style header

--- src/App.js
+++ src/App.js
@@ -18,8 +18,8 @@ const App = () => {
       }}
       >
         <Router>
-          <header>
-            <Link to="/">
+          <header className="w-full mb-10 text-center p-7 bg-gradient-to-b from-purple-400 via-pink-500 to-red-500">
+            <Link to="/" className="text-6xl text-white hover:text-gray-200">
               <h1>Adopt Me!</h1>
             </Link>
           </header>
