f08b8ed8a729c7ff92c4c102be46b3a951012c5a
Widzmarc Jean Nesly Phelle <widzmarc@gmail.com>
2021-09-08 15:01:56 -0400

    Implement streaming markup

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 /* eslint-disable import/namespace */
 /* eslint-disable import/named */
 import express from "express";
-import { renderToString } from "react-dom/server";
+import { renderToNodeStream } from "react-dom/server";
 import { StaticRouter } from "react-router-dom";
 import fs from "fs";
 import App from "../src/App";
@@ -16,6 +16,7 @@ const app = express();
 
 app.use("/dist", express.static("dist"));
 app.use((req, res) => {
+  res.write(parts[0]);
   const staticContext = {};
   const reactMarkup = (
     <StaticRouter url={req.url} context={staticContext}>
@@ -23,9 +24,13 @@ app.use((req, res) => {
     </StaticRouter>
   );
 
-  res.status(staticContext.statusCode || 200);
-  res.send(`${parts[0]}${renderToString(reactMarkup)}${parts[1]}`);
-  res.end();
+  const stream = renderToNodeStream(reactMarkup);
+  stream.pipe(res, { end: false });
+  stream.on("end", () => {
+      res.status(staticContext || 200);
+      res.write(parts[1]);
+      res.end();
+  });
 });
 
 console.log(`listening on http://localhost:${PORT}`);
