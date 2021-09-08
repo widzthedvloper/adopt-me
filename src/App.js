6547162637a3def7b1a4f45a74b366fe55df43e5
Widzmarc Jean Nesly Phelle <widzmarc@gmail.com>
2021-09-08 13:03:24 -0400

    Import lazy and suspense

--- src/App.js
+++ src/App.js
@@ -1,32 +1,12 @@
 /* eslint-disable import/namespace */
 /* eslint-disable import/named */
-import React, { StrictMode, useState } from "react";
+import React, { StrictMode, useState , lazy, Suspense} from "react";
 import ReactDOM from "react-dom";
-import SearchParams from "./SearchParams";
-import Details from "./Details";
 import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
 import ThemeContext from "./ThemeContext";
 
-// const App = () => {
-//   return React.createElement("div", {}, [
-//     React.createElement("h1", {}, "Adopt Me!"),
-//     React.createElement(Pet, {
-//       name: "Luna",
-//       animal: "Dog",
-//       breed: "Havanese",
-//     }),
-//     React.createElement(Pet, {
-//       name: "Pepper",
-//       animal: "Bird",
-//       breed: "Cockatiel",
-//     }),
-//     React.createElement(Pet, {
-//       name: "Sudo",
-//       animal: "Dog",
-//       breed: "Wheaten Terrier",
-//     }),
-//   ]);
-// };
+const Details = lazy(() => import("./Details"));
+const SearchParams = lazy(() => import("./SearchParams"));
 
 const App = () => {
   const theme = useState("darkblue");
@@ -34,21 +14,23 @@ const App = () => {
   return (
     <ThemeContext.Provider value={theme}>
       <div>
-        <Router>
-          <header>
-            <Link to="/">
-              <h1>Adopt Me!</h1>
-            </Link>
-          </header>
-          <Switch>
-            <Route path="/details/:id">
-              <Details />
-            </Route>
-            <Route path="/">
-              <SearchParams />
-            </Route>
-          </Switch>
-        </Router>
+        <Suspense fallback={<h2>Loding route ...</h2>}>
+          <Router>
+            <header>
+              <Link to="/">
+                <h1>Adopt Me!</h1>
+              </Link>
+            </header>
+            <Switch>
+              <Route path="/details/:id">
+                <Details />
+              </Route>
+              <Route path="/">
+                <SearchParams />
+              </Route>
+            </Switch>
+          </Router>
+        </Suspense>
       </div>
     </ThemeContext.Provider>
   );
